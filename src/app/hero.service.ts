edfb37db964d1659ff179bc4e5f896de532f2db0
Thomas <thomas@osynlig.se>
2018-10-02 11:35:07 +0200

    Add routing to navigate around

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,7 +16,13 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message -after- fetching the heroes
-    this.messageService.add('heroService: fetched heroes');
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message -after- fetching the heroes
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
